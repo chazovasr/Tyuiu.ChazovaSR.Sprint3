4512fe29d8889607fad82b284cc53e097f78aa18
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-10-23 19:01:28 +0500

    Task4

---
+++ Tyuiu.ChazovaSR.Sprint3.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ChazovaSR.Sprint3.Task4.V14.Lib;
+
+namespace Tyuiu.ChazovaSR.Sprint3.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 6.263;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
